9eb2662bc88cf566b63a07565ac931a0239d96ed
Fancy <553973204@qq.com>
2019-07-04 17:11:49 +0800

    merge

---
+++ materials.microservice/target/materials.microservice-0.0.1-SNAPSHOT/WEB-INF/classes/sql/MaterialInventory.sql
@@ -0,0 +1,14 @@
+/**
+ * PLEASE KEEP THIS INFOMATION
+ * CREATE BY AVATECH EDI CODE TOOL
+ * AT 2019-07-01
+ */
+        CREATE TABLE AVA_IM_OITW(
+                    "ItemCode" NVARCHAR(8) ,
+                    "ItemName" NVARCHAR(30) ,
+                    "GroupCode" DECIMAL(8) ,
+                    "GroupName" NVARCHAR(20) ,
+                    "PackUnit" NVARCHAR(20) ,
+                    "OnHand" INTEGER(20) ,
+                    "Price" INTEGER(20) 
+        );
