11caab63c9bf69a110dc44cd84f4607ad9d28656
Fancy <553973204@qq.com>
2019-07-04 17:08:55 +0800

    修改打包方式（jar包方式变更为war包）

--- materials.microservice/src/main/java/com/avatech/edi/materials/MaterialsApplication.java
+++ materials.microservice/src/main/java/com/avatech/edi/materials/MaterialsApplication.java
@@ -8,16 +8,22 @@ package com.avatech.edi.materials;
 import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 import org.springframework.context.annotation.Bean;
 import org.springframework.scheduling.annotation.EnableScheduling;
 import org.springframework.web.client.RestTemplate;
 
 @SpringBootApplication
 @MapperScan(basePackages = "com.avatech.edi.materials.mapper")
-public class MaterialsApplication {
+public class MaterialsApplication extends SpringBootServletInitializer {
 
     public static void main(String[] args) {
         SpringApplication.run(MaterialsApplication.class, args);
     }
 
+    @Override
+    protected SpringApplicationBuilder configure(SpringApplicationBuilder builder) {
+        return builder.sources(MaterialsApplication.class);
+    }
 }
--- salesdeliveries.microservice/src/main/java/com/avatech/edi/salesdeliveries/SalesdeliveriesApplication.java
+++ salesdeliveries.microservice/src/main/java/com/avatech/edi/salesdeliveries/SalesdeliveriesApplication.java
@@ -8,6 +8,8 @@ package com.avatech.edi.salesdeliveries;
 import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 import org.springframework.context.annotation.Bean;
 import org.springframework.scheduling.annotation.EnableScheduling;
 import org.springframework.web.client.RestTemplate;
@@ -15,10 +17,14 @@ import org.springframework.web.client.RestTemplate;
 @SpringBootApplication
 @EnableScheduling
 @MapperScan(basePackages = "com.avatech.edi.salesdeliveries.mapper")
-public class SalesdeliveriesApplication {
+public class SalesdeliveriesApplication extends SpringBootServletInitializer {
 
     public static void main(String[] args) {
         SpringApplication.run(SalesdeliveriesApplication.class, args);
     }
 
+    @Override
+    protected SpringApplicationBuilder configure(SpringApplicationBuilder builder) {
+        return builder.sources(SalesdeliveriesApplication.class);
+    }
 }
